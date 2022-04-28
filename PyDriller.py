e3c91e61988284d6c4f9837b5f2bb0ccc728bf82
srinidhipatil1 <srinidhi.patil.39@gmail.com>
2022-04-27 22:15:04 -0500

    update1

--- PyDriller.py
+++ PyDriller.py
@@ -1,20 +1,31 @@
 from pydriller import Repository
 import re
+import csv
 import pandas as pd
 from matplotlib import pyplot as plt
 
-f = open("data/PyDriller.csv", "w")
-f1 = open("data/PyDrillerCommitMessages.csv", "w")
+# f = open("data/PyDriller.csv", "w")
+# f1 = open("data/PyDrillerCommitMessages.csv", "w")
+
+
+
 d = {}
 commitMessages = {}
-f.write("filepath, author_names, date_added, num_of_contributors, how_often_modified\n")
-f1.write("code, number_of_occurences\n")
+authorNames = {}
+# f.write("filepath, author_names, date_added, num_of_contributors, how_often_modified\n")
+# f1.write("code, number_of_occurences\n")
 
 for commit in Repository(path_to_repo='https://github.com/numpy/numpy').traverse_commits():
     # f.write("-----------------------------------\n")
     # changecount = 0
     # f.write(commit.hash)
     # f.write(commit.msg)
+
+    if commit.author.name not in authorNames:
+        authorNames[commit.author.name] = 1
+    else:
+        authorNames[commit.author.name] += 1
+
     if bool(re.match('bug', commit.msg, re.I)):
         if 'bug' not in commitMessages:
             commitMessages['bug'] = 1
@@ -52,6 +63,8 @@ for commit in Repository(path_to_repo='https://github.com/numpy/numpy').traverse
             commitMessages['build'] += 1
 
 
+
+
     for file in commit.modified_files:
         # changecount += 1
         # if file.new_path is not None:
@@ -80,11 +93,35 @@ for commit in Repository(path_to_repo='https://github.com/numpy/numpy').traverse
             d[file.new_path][2] = (len(d[file.new_path][0]))
             d[file.new_path][3] = (len(d[file.new_path][1]))
 
-for key in d:
-    d[key][1] = d[key][1][0]
-    StrListNames = ','.join(str(e) for e in d[key][0])
-    f.write('{}, {}, {}, {}, {}\n'.format(key, StrListNames, d[key][1], d[key][2], d[key][3]))
 
-for key1 in commitMessages:
-    f1.write('{}, {}\n'.format(key1, commitMessages[key1]))
+with open('data/PyDriller.csv', 'w') as csv_file:
+    # header1 = ['filepath', 'author_names', 'date_added', 'num_of_contributors', 'how_often_modified']
+    writer1 = csv.writer(csv_file)
+    writer1.writerow(['filepath', 'author_names', 'date_added', 'num_of_contributors', 'how_often_modified'])
+
+    for key in d:
+        d[key][1] = d[key][1][0]
+        StrListNames = ','.join(str(e) for e in d[key][0])
+        # f.write('{}, {}, {}, {}, {}\n'.format(key, StrListNames, d[key][1], d[key][2], d[key][3]))
+        writer1.writerow([key, StrListNames, d[key][1], d[key][2], d[key][3]])
+
+with open('data/PyDrillerCommitMessages.csv', 'w') as csv_file:
+    writer2 = csv.writer(csv_file)
+    writer2.writerow(['code', 'num_of_occurrences'])
+
+    for key1 in commitMessages:
+        writer2.writerow([key1, commitMessages[key1]])
+
+with open('data/PyDrillerAuthors.csv', 'w') as csv_file:
+    writer3 = csv.writer(csv_file)
+    writer3.writerow(['names', 'count'])
+    for key2 in authorNames:
+        writer3.writerow([key2, authorNames[key2]])
+
+
+
+
+
+
+
 
