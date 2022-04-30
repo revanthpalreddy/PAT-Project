87d19e97e96f1721c3116ca2792c0753f1449a75
Sreya Bhattacharya <77694442+sreya2906@users.noreply.github.com>
2022-04-30 18:24:34 -0500

    Update test-coverage.py

--- test-coverage.py
+++ test-coverage.py
@@ -1,7 +1,13 @@
 #!/usr/bin/env python3
 
 import os
-os.system('pip install -r numpy-git/doc_requirements.txt -r numpy-git/linter_requirements.txt -r numpy-git/release_requirements.txt -r numpy-git/test_requirements.txt');
-os.system('coverage run --branch numpy-git/runtests.py');
+os.system('pip install gitpython')
+from git import Repo
+from git import rmtree
+
+Repo.clone_from('https://github.com/numpy/numpy.git', 'numpy-public-git')
+os.system('pip install -r numpy-public-git/doc_requirements.txt -r numpy-public-git/linter_requirements.txt -r numpy-public-git/release_requirements.txt -r numpy-public-git/test_requirements.txt');
+os.system('coverage run --branch numpy-public-git/runtests.py');
 os.system('coverage json --pretty-print -o json-coverage/coverage.json ');
 os.system('coverage html -d html-coverage');
+rmtree('numpy-public-git')
